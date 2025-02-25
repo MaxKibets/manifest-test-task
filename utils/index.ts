880e7700755d8beeed2818cb2c290d7eae98d183
MaxKibets <kibetss3@gmail.com>
2025-02-25 10:25:13 +0200

    [add]: utils: useMediaQuery

---
+++ utils/index.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export default function useMediaQuery(mediaQuery: string) {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const mediaQueryList = window.matchMedia(mediaQuery);
+
+    const handleChange = (event: MediaQueryListEvent | MediaQueryList) => {
+      setMatches(event.matches);
+    };
+
+    handleChange(mediaQueryList);
+
+    mediaQueryList.addEventListener("change", handleChange, {
+      signal: controller.signal,
+    });
+
+    return () => {
+      controller.abort();
+    };
+  }, [mediaQuery]);
+
+  return matches;
+}
