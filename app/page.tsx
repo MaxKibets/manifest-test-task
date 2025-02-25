f0cf6f85b26c7d0c805ac766d6efd6f6cf01ad15
MaxKibets <kibetss3@gmail.com>
2025-02-25 17:49:59 +0200

    [change]: global styles + main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import Heading from "@/components/Heading";
+import InstrumentsList from "@/components/InstrumentsList";
+import Timer from "@/components/Timer";
+import Packages from "@/components/Packages";
+import TimerProvider from "@/components/TimerContext";
+import Button from "@/components/Button";
+import Terms from "@/components/Terms";
+import PackageProvider from "@/components/PackageContext";
+
 export default function Home() {
-  return <div>main page</div>;
+  return (
+    <main>
+      <section>
+        <Heading>Choose Your Plan:</Heading>
+        <InstrumentsList />
+        <PackageProvider>
+          <TimerProvider>
+            <Timer mediaQuery={"(max-width: 1024px)"} />
+            <Packages />
+          </TimerProvider>
+          <Button>Get Started</Button>
+          <Terms />
+        </PackageProvider>
+      </section>
+    </main>
+  );
 }
